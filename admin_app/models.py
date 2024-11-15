8dc4471a245e2324c3761f43c2eea9bd99c73c22
SarthakGadge <sarthakgadge43@gmail.com>
2024-11-15 12:45:01 +0530

    change admin_feedback_manager&employee

--- admin_app/models.py
+++ admin_app/models.py
@@ -6,8 +6,6 @@ class AdminFeedbackManager(models.Model):
     manager = models.ForeignKey(Manager, on_delete=models.CASCADE)
     rating = models.FloatField()
     feedback = models.TextField(null=True, blank=True)
-    direct_report_feedback = models.TextField(null=True, blank=True)
-    overall_review = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.manager} - Rating: {self.rating}"
@@ -17,8 +15,6 @@ class AdminFeedbackEmployee(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     rating = models.FloatField()
     feedback = models.TextField(null=True, blank=True)
-    direct_report_feedback = models.TextField(null=True, blank=True)
-    overall_review = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.employee} - Rating: {self.rating}"
